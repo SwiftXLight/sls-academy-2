41374d89f0a9d4cf75fff4217b4d5130f99610bc
Andrii Panchenko <andrevpanchenko@gmail.com>
2023-06-03 18:12:17 +0300

    feat: make instagram giveaway

---
+++ 06_instagram_giveaway/index.js
@@ -0,0 +1,116 @@
+const fs = require('fs');
+const path = require('path');
+const { performance } = require('perf_hooks');
+const { Transform } = require('stream');
+
+(async () => {
+  function readUsernamesFromFile(filePath) {
+    const readStream = fs.createReadStream(filePath, 'utf-8');
+    const transformStream = new Transform({
+      readableObjectMode: true,
+      transform(chunk, encoding, callback) {
+        const usernames = chunk.toString().split('\n');
+        for (const username of usernames) {
+          this.push(username);
+        }
+        callback();
+      }
+    });
+
+    readStream.pipe(transformStream);
+    return transformStream;
+  }
+
+  function uniqueValues() {
+    return new Promise((resolve, reject) => {
+      const usernamesSet = new Set();
+      const filesPath = './files';
+
+      const filePromises = Array.from({ length: 20 }, (_, i) => {
+        const filePath = path.join(filesPath, `out${i}.txt`);
+        return new Promise((fileResolve, fileReject) => {
+          const usernamesStream = readUsernamesFromFile(filePath);
+          usernamesStream.on('data', (username) => {
+            usernamesSet.add(username);
+          });
+          usernamesStream.on('end', () => fileResolve());
+          usernamesStream.on('error', (error) => fileReject(error));
+        });
+      });
+
+      Promise.all(filePromises)
+        .then(() => resolve(usernamesSet.size))
+        .catch(reject);
+    });
+  }
+
+  function existInAllFiles() {
+    return new Promise((resolve, reject) => {
+      const filesPath = './files';
+      const commonUsernames = new Set();
+
+      const filePath = path.join(filesPath, 'out0.txt');
+      const usernamesStream = readUsernamesFromFile(filePath);
+      usernamesStream.on('data', (username) => {
+        commonUsernames.add(username);
+      });
+      usernamesStream.on('end', () => {
+        for (let i = 1; i < 20; i++) {
+          const filePath = path.join(filesPath, `out${i}.txt`);
+          const usernamesStream = readUsernamesFromFile(filePath);
+          usernamesStream.on('data', (username) => {
+            if (!commonUsernames.has(username)) {
+              commonUsernames.delete(username);
+            }
+          });
+          usernamesStream.on('end', () => resolve(commonUsernames.size));
+          usernamesStream.on('error', (error) => reject(error));
+        }
+      });
+      usernamesStream.on('error', (error) => reject(error));
+    });
+  }
+
+  function existInAtleastTen() {
+    return new Promise((resolve, reject) => {
+      const filesPath = './files';
+      const usernamesCountMap = new Map();
+
+      const filePromises = Array.from({ length: 20 }, (_, i) => {
+        const filePath = path.join(filesPath, `out${i}.txt`);
+        return new Promise((fileResolve, fileReject) => {
+          const usernamesStream = readUsernamesFromFile(filePath);
+          usernamesStream.on('data', (username) => {
+            const count = usernamesCountMap.get(username) || 0;
+            usernamesCountMap.set(username, count + 1);
+          });
+          usernamesStream.on('end', () => fileResolve());
+          usernamesStream.on('error', (error) => fileReject(error));
+        });
+      });
+
+      Promise.all(filePromises)
+        .then(() => {
+          let count = 0;
+          for (const occurrence of usernamesCountMap.values()) {
+            if (occurrence >= 10) {
+              count++;
+            }
+          }
+          resolve(count);
+        })
+        .catch(reject);
+    });
+  }
+
+  const startTime = performance.now();
+  const uniqueUsernames = await uniqueValues();
+  const allFilesUsernames = await existInAllFiles();
+  const tenFilesUsernames = await existInAtleastTen();
+  const endTime = performance.now();
+
+  console.log('Unique Usernames:', uniqueUsernames);
+  console.log('Usernames in All Files:', allFilesUsernames);
+  console.log('Usernames in at Least 10 Files:', tenFilesUsernames);
+  console.log('Elapsed Time:', endTime - startTime, 'ms');
+})();
